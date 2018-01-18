ecfc4bada0709ca7012f652f77f1290848c7facb
Flaviano Dias (@flavindias) <flavindias@msn.com>
2018-01-17 21:31:34 -0300

    tableviewcell

---
+++ Turismo/LocalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocalTableViewCell.swift
+//  Turismo
+//
+//  Created by Flaviano Dias Fontes on 17/01/18.
+//  Copyright © 2018 Flaviano Dias Fontes. All rights reserved.
+//
+
+import UIKit
+
+class LocalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
